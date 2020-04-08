faccb690e9da834f5c27f35451e0e9e8f0b48405
ccomangi2 <ccomangi2@gmail.com>
2020-04-08 22:44:50 +0900

    프로필 수정 화면 액티비티 추가

---
+++ app/src/main/java/com/dimirim/minorhobby/ui/editprofile/EditProfileActivity.java
@@ -0,0 +1,16 @@
+package com.dimirim.minorhobby.ui.editprofile;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.dimirim.minorhobby.R;
+
+public class EditProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editprofile);
+    }
+}
